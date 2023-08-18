3ecbbf5e125193bd7af75fce970c38a763a4008d
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-08-18 20:17:28 -0300

    range

--- section_03(strating)/01.py
+++ section_03(strating)/01.py
@@ -14,9 +14,6 @@ print(f'Seu nome tem {len(nome)} letras.')
 print(f'A primeira letra do seu nome é {nome[0]}.')
 print(f'A última letra do seu nome é {nome[len(nome)-1]}.')
 
-numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-for numero in numeros:
-    print(f'{numero} elevado ao quadrado é {numero ** 2}.')
         
+
 
---
+++ section_03(strating)/02.py
@@ -0,0 +1,4 @@
+numeros = range(10)
+
+for numero in numeros:
+    print(f'{numero} elevado ao quadrado é {numero ** 2}.')
